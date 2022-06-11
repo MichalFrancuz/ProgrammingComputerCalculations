318f7f6bf261699763c678649fc3e9fbd3fbbbb5
Michal <sasxcxc@icloud.com>
2022-06-11 22:00:50 +0200

    Rozwiązane zad 1 ćwi 1 Programowanie obliczeń komputerowych. Michał Francuz, sasxcxc@icloud.com

---
+++ src/Cwiczenia1/Zad1.java
@@ -0,0 +1,11 @@
+package Cwiczenia1;
+import static java.lang.Math.*;
+
+public class Zad1{
+    public static void main(String[]args){
+       int l=4;
+       int doIlu=3;
+       double objetosc=pow(l,doIlu);
+       System.out.println("Objetosc szescianu wynosi"+objetosc+".");
+    }
+}
