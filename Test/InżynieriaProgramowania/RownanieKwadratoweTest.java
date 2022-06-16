f359accc10defb58d4778408b588ac8236ab5427
Michal <sasxcxc@icloud.com>
2022-06-16 20:15:46 +0200

    Inżynieria Oprogramowania, rownanie kwadratowe - dodanie testow biblioteka junit4. Michał Francuz, sasxcxc@icloud.com

---
+++ Test/InżynieriaProgramowania/RownanieKwadratoweTest.java
@@ -0,0 +1,16 @@
+package InżynieriaProgramowania;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RownanieKwadratoweTest {
+
+    @Test
+    public void main() {
+    }
+
+    @Test
+    public void rownanieLiniowe() {
+    }
+}
