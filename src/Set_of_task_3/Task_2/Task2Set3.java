e760fe67b7e425a394228da371f4fdc17349f9cf
Michal <sasxcxc@icloud.com>
2022-09-29 23:49:19 +0200

    Michał Francuz, francuz.michal@icloud.com, Task2Set3 is done and Task3Set3 is almost done.

--- src/Set_of_task_3/Task_2/Task2Set3.java
+++ src/Set_of_task_3/Task_2/Task2Set3.java
@@ -1,4 +1,35 @@
 package Set_of_task_3.Task_2;
 
 public class Task2Set3 {
+    static double[][] matrix1 = {{1, 2, 3}, // 1, 2
+                                 {2, 3, 4}, // 2, 3
+                                 {3, 4, 5}}; // 3, 4
+    static double[][] matrix2 = {{2.11, -0.8, 1.72}, // 2.11, -0.8
+                                {-1.84, 3.03, 1.29}, // -1.84, 3.03
+                                 {-1.57, 5.25, 4.3}}; // -1.57, 5.25
+    static double[][] matrix3 = {{2, -1, 0}, // 2, -1
+                                {-1, 2, -1}, // -1, 2
+                                 {0, -1, 2}}; // 0, -1
+    static double[][] matrix4 = {{4, 3, -1}, // 4, 3
+                                 {7, -2, 3}, // 7, -2
+                               {5, -18, 13}}; // 5, -18
+
+    public static void main(String[] args) {
+        System.out.println(calculateMatrix3x3(matrix1)+"\n"); // singular and well-conditioned matrix
+        System.out.println(calculateMatrix3x3(matrix2)+"\n"); // non-singular and ill-conditioned matrix
+        System.out.println(calculateMatrix3x3(matrix3)+"\n"); // non-singular and well-conditioned matrix
+        System.out.println(calculateMatrix3x3(matrix4)+"\n"); // singular and well-conditioned matrix
+    }
+
+    public static double calculateMatrix3x3(double[][] x) {
+        double firstFromLeft = x[0][0] * x[1][1] * x[2][2];
+        double secondFromLeft = x[0][1] * x[1][2] * x[2][0];
+        double thirdFromLeft = x[0][2] * x[1][0] * x[2][1];
+        double firstFromRight = x[0][1] * x[1][0] * x[2][2];
+        double secondFromRight = x[0][0] * x[1][2] * x[2][1];
+        double thirdFromRight = x[0][2] * x[1][1] * x[2][0];
+        double leftResume = firstFromLeft + secondFromLeft + thirdFromLeft;
+        double rightResume = firstFromRight + secondFromRight + thirdFromRight;
+        return leftResume - rightResume;
+    }
 }
--- src/Set_of_task_3/Task_3/Task3Set3.java
+++ src/Set_of_task_3/Task_3/Task3Set3.java
@@ -1,4 +1,38 @@
 package Set_of_task_3.Task_3;
 
+import java.util.Arrays;
+
 public class Task3Set3 {
+    static double[][] matrix1L = {{1, 0, 0},
+            {1, 1, 0},
+            {1, 5 / 3, 1}};
+    static double[][] matrix1U = {{1, 2, 4},
+            {0, 3, 21},
+            {0, 0, 0}};
+    static double[][] matrix2L = {{2, 0, 0},
+            {-1, 1, 0},
+            {1, -3, 1}};
+    static double[][] matrix2U = {{2, -1, 1},
+            {0, 1, -3},
+            {0, 0, 1}};
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.deepToString(calculateMultiplication(matrix1L, matrix1U)));
+        System.out.println(Arrays.deepToString(calculateMultiplication(matrix2L, matrix2U)));
+    }
+
+    public static double[][] calculateMultiplication(double[][] x, double[][] y) {
+        double upLeft = x[0][0] * y[0][0] + x[0][1] * y[1][0] + x[0][2] * y[2][0];
+        double upMiddle = x[0][0] * y[0][1] + x[0][1] * y[1][1] + x[0][2] * y[2][1];
+        double upRight = x[0][0] * y[0][2] + x[0][1] * y[1][2] + x[0][2] * y[2][2];
+        double middleLeft = x[1][0] * y[0][0] + x[1][1] * y[1][0] + x[1][2] * y[2][0];
+        double middleMiddle = x[1][0] * y[0][1] + x[1][1] * y[1][1] + x[1][2] * y[2][1];
+        double middleRight = x[1][0] * y[0][2] + x[1][1] * y[1][2] + x[1][2] * y[2][2];
+        double downLeft = x[2][0] * y[0][0] + x[2][1] * y[1][0] + x[2][2] * y[2][0];
+        double downMiddle = x[2][0] * y[0][1] + x[2][1] * y[1][1] + x[2][2] * y[2][1];
+        double downRight = x[2][0] * y[0][2] + x[2][1] * y[1][2] + x[2][2] * y[2][2];
+        return new double[][]{{upLeft, upMiddle, upRight},
+                {middleLeft, middleMiddle, middleRight},
+                {downLeft, downMiddle, downRight}};
+    }
 }
