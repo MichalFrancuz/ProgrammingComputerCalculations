7880fbfc4b610f8a83160e602c0f61f6278d12c9
Michal <sasxcxc@icloud.com>
2023-01-22 23:43:00 +0100

    Michał Francuz, francuz.michal@icloud.com, Task2set1 - translate to english

--- src/Set_of_task_1/Task_2/Task2Set1.java
+++ src/Set_of_task_1/Task_2/Task2Set1.java
@@ -4,14 +4,14 @@ import static java.lang.Math.PI;
 
 public class Task2Set1 {
     public static void main(String[] args) {
-        double dobryWynikCiA = 12.566370614359172;
+        double goodResultCAndA = 12.566370614359172;
         int r = 2;
         double C = 2 * PI * r;
         double A = PI * (r * r);
         System.out.println(A);
-        if (C == dobryWynikCiA && A == dobryWynikCiA)
-            System.out.println("Obwod kola (C) wynosi: " + C + " , a pole kola (A) wynosi: " + A);
+        if (C == goodResultCAndA && A == goodResultCAndA)
+            System.out.println("The circumference of the circle (C) is: " + C + " , and the surface of the circle (A) is: " + A);
         else
-            System.out.println("Obliczenia sa zle lub zostaly zmienione zmienne, wynik jest dla zmiennych r=2, i PI dokładnego do 15. liczby po przecinku.");
+            System.out.println("The calculation is wrong or the variables have been changed, the result is for variables r=2, and PI with accurate to 15. decimal place.");
     }
 }
