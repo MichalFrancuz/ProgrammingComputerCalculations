8536f9a7b9af0b1a8f30973b6a4a5f4e2f56eb13
Michal <sasxcxc@icloud.com>
2023-02-01 23:09:51 +0100

    Michał Francuz, francuz.michal@icloud.com, Task1Set1 - translate to english

--- src/Set_of_task_1/Task_1/Task1Set1.java
+++ src/Set_of_task_1/Task_1/Task1Set1.java
@@ -5,8 +5,8 @@ import static java.lang.Math.*;
 public class Task1Set1 {
     public static void main(String[] args) {
         int l = 4;
-        int doIlu = 3;
-        double objetosc = pow(l, doIlu);
-        System.out.println("Objetosc szescianu wynosi " + objetosc + " .");
+        int howMuch = 3;
+        double volume = pow(l, howMuch);
+        System.out.println("The volume of the cube is " + volume + " .");
     }
 }
