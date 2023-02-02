812bf4f42577d40fa1e49e251e6ca8e7a7dc47e6
Michal <sasxcxc@icloud.com>
2023-02-02 19:58:56 +0100

    Michał Francuz, francuz.michal@icloud.com, Task1Set3 - translate to english

--- src/Set_of_task_1/Task_3/Task3Set1.java
+++ src/Set_of_task_1/Task_3/Task3Set1.java
@@ -3,11 +3,11 @@ package Set_of_task_1.Task_3;
 import java.util.Arrays;
 
 public class Task3Set1 {
-    public static strictfp double[] metoda(double d1, double d2, int n, double potega) {
+    public static strictfp double[] metod(double d1, double d2, int n, double power) {
         double[] y = new double[n];
         double[] p = linspace(d1, d2, n);
-        for (int i = 0; i < y.length - 1; i++) y[i] = Math.pow(potega, p[i]);
-        y[y.length - 1] = Math.pow(potega, d2);
+        for (int i = 0; i < y.length - 1; i++) y[i] = Math.pow(power, p[i]);
+        y[y.length - 1] = Math.pow(power, d2);
         return y;
     }
 
@@ -19,9 +19,9 @@ public class Task3Set1 {
     }
 
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(metoda(1,3,3,1)));
+        System.out.println(Arrays.toString(metod(1,3,3,1)));
         System.out.println(Arrays.toString(linspace(1,3,3)));
-        System.out.println(Arrays.toString(metoda(1,3,3,2)));
-        System.out.println(Arrays.toString(metoda(1,3,3,3)));
+        System.out.println(Arrays.toString(metod(1,3,3,2)));
+        System.out.println(Arrays.toString(metod(1,3,3,3)));
     }
 }
